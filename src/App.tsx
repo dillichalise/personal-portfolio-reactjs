1e6d3d47302ddd8ba787ae7800e326f60b1105f0
Dilli Chalise <dillichalise@gmail.com>
2023-06-06 20:29:57 +0545

    CSS for navbar implemented, footer added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./header";
+import Footer from "./footer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
