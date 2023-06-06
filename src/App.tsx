c8ee29f9d7e79609d3897451bee948743c136fa6
Dilli Chalise <dillichalise@gmail.com>
2023-06-06 23:01:56 +0545

    Intro component added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./header";
 import Footer from "./footer";
+import IntroPage from "./intro";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <IntroPage />
       <Footer />
     </div>
   );
---
+++ src/intro/index.tsx
@@ -0,0 +1,40 @@
+const IntroPage = () => {
+  return (
+    <div className="profile-page">
+      <div className="page-header page-header-small">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: `url("https://www.dillichalise.com.np/assets/images/intro.jpg")`,
+          }}
+        />
+        <div className="container">
+          <div className="content-center">
+            <div className="cc-profile-image">
+              <a href="#">
+                <img
+                  src="https://www.dillichalise.com.np/assets/images/dillichalise.jpg"
+                  alt="Profile"
+                />
+              </a>
+            </div>
+            <div className="h2 title">Dilli Raj Chalise</div>
+            <p className="category text-white">Web Developer</p>
+            <a className="btn btn-primary mr-2" href="#contact">
+              Hire Me
+            </a>
+            <a
+              className="btn btn-primary"
+              href="https://drive.google.com/file/d/1L2JnV9XsHb1YTzzPorhwLwkyBHtCIEGF/view"
+              target="_blank"
+            >
+              View CV
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroPage;
